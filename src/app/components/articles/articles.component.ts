294fa672b690b6bfe5638ff668af0adea9a69fe0
YoussefShaban2406 <Youssef.Shaban2406@gmail.com>
2026-02-16 17:30:34 +0200

    Adding Feature of Grid and List View in Articles Section

--- src/app/components/articles/articles.component.ts
+++ src/app/components/articles/articles.component.ts
@@ -1,14 +1,16 @@
-import { Component, inject, Input} from '@angular/core';
+import { Component, ElementRef, inject, Input, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { Posts } from '../../services/posts';
 
 export enum Categories {
-    All,
-    Light,
-    portrait,
-    Sights,
-    Tech,
-    Appliances
-  };
+  All,
+  Light,
+  portrait,
+  Sights,
+  Tech,
+  Appliances
+};
+
+type articleSectionView = 'Grid' | 'List';
 
 @Component({
   selector: 'app-articles',
@@ -18,7 +20,24 @@ export enum Categories {
 })
 export class ArticlesComponent {
   data = inject(Posts);
-  @Input({required:true}) categ!:Number;
+  @Input({ required: true }) categ!: Number;
+  @Input({ required: true }) categLength!: Number;
   Categories = Categories;
 
+  articlesection = true;
+  articleSectionLine = false;
+  flag = true
+
+  onSetView(view: articleSectionView){
+    if(view === 'Grid'){
+      this.articlesection = true;
+      this.articleSectionLine = false;
+      this.flag = true;
+    }
+    else{
+      this.articlesection = false;
+      this.articleSectionLine = true;
+      this.flag = false;
+    }
+  }
 }
--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
+import { Component, ElementRef, inject, QueryList, ViewChildren } from '@angular/core';
 import { NgClass } from '@angular/common';
 import { ArticlesComponent } from "../articles/articles.component";
+import { Posts } from '../../services/posts';
 
 @Component({
   selector: 'app-blog',
@@ -9,18 +10,61 @@ import { ArticlesComponent } from "../articles/articles.component";
   styleUrl: './blog.component.css',
 })
 export class BlogComponent {
+  data = inject(Posts);
   currentCategory = 0;
-  @ViewChildren('articleCategory') articleCategory!: QueryList<ElementRef>;  
-  ngAfterViewInit(){
+  currentCategoryLength: number = this.data.postsArray.posts.length;
+  @ViewChildren('articleCategory') articleCategory!: QueryList<ElementRef>;
+
+  ngAfterViewInit() {
     this.articleCategory.forEach((element) => {
       (element.nativeElement as HTMLElement).addEventListener('click', () => {
         this.articleCategory.forEach((el) => {
           el.nativeElement.classList.remove('article-categories-active');
         })
         this.currentCategory = +element.nativeElement.getAttribute('aria-label');
+        this.categorySelectionLength(this.currentCategory);
         element.nativeElement.classList.add('article-categories-active');
       });
     })
-   }
+  }
+
+  categorySelectionLength(category: number) {
+    let filteredPosts: any[] = [];
+    switch (category) {
+      case 0:
+        this.currentCategoryLength = this.data.postsArray.posts.length;
+        break;
+      case 1:
+        filteredPosts = this.data.postsArray.posts.filter(
+          (el) => el.category === 'إضاءة'
+        );
+        this.currentCategoryLength = filteredPosts.length;
+        break;
+      case 2:
+        filteredPosts = this.data.postsArray.posts.filter(
+          (el) => el.category === 'بورتريه'
+        );
+        this.currentCategoryLength = filteredPosts.length;
+        break;
+      case 3:
+        filteredPosts = this.data.postsArray.posts.filter(
+          (el) => el.category === 'مناظر طبيعية'
+        );
+        this.currentCategoryLength = filteredPosts.length;
+        break;
+      case 4:
+        filteredPosts = this.data.postsArray.posts.filter(
+          (el) => el.category === 'تقنيات'
+        );
+        this.currentCategoryLength = filteredPosts.length;
+        break;
+      case 5:
+        filteredPosts = this.data.postsArray.posts.filter(
+          (el) => el.category === 'معدات'
+        );
+        this.currentCategoryLength = filteredPosts.length;
+        break;
+    }
+  }
 
 }
